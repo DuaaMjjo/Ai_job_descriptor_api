c71cd7c66997d8b6409e4fe7abb057ded6c33854
Duaa Mjjo <75542436+DuaaMjjo@users.noreply.github.com>
2023-09-18 06:17:25 -0700

    JobDescriptors.py

--- JobDescriptors.py
+++ JobDescriptors.py
@@ -3,7 +3,7 @@ from langchain.llms import OpenAI
 from langchain import PromptTemplate
 from langchain.chains import LLMChain
 
-os.environ['OPENAI_API_KEY'] = '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'
+os.environ['OPENAI_API_KEY'] = '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'
 
 
 class JobDescriptor:
