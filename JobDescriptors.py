1f6aa110d71ddb704b26327c32ea3fed048503a2
Duaa Mjjo <75542436+DuaaMjjo@users.noreply.github.com>
2023-09-24 10:34:36 -0700

    JobDescriptors.py

--- JobDescriptors.py
+++ JobDescriptors.py
@@ -8,28 +8,33 @@ os.environ.get("OPENAI_API_KEY")
 class JobDescriptor:
     llm = OpenAI(model_name='text-davinci-003',
                  temperature=0.9,
-                 max_tokens=1000)
+                 max_tokens=4000)
 
-    # @title Prompt Template
-
+    # @title Prompt Templat
     job_description_template = """
     I want you to act as a writer. write about job description.
-    
-    Return job description.
-    
-    the name of the company is {name_of_company}.
-    
-    job Position is {job_Position}.
-    
-    the years of experience required {year_of_experience}.
-    
+
+    format the output like the following:
+
+    Job Title: {job_title}
+
+    Company: {name_of_company}
+
+    Job Type: {job_type}
+
+    Location: {location}
+
+    Experience: {year_of_experience}
+
+    then Return job description with Responsibilities and Qualifications.
+
     """
 
     prompt_template = PromptTemplate(
-        input_variables=["name_of_company", "job_Position", "year_of_experience"],
+        input_variables=["name_of_company", "job_title", "job_type", "location", "year_of_experience"],
         template=job_description_template,
     )
-
+    
     # querying the model with the prompt template
     my_chain = LLMChain(llm=llm, prompt=prompt_template)
 
